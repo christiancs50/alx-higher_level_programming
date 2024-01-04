bdffa670ef1c906645648725b38a08b115c284d2
“christiancs50” <christian.gaoso@gmail.com>
2024-01-04 12:12:07 +0000

    prtotypes

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,15 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+
+
+def uppercase(str);
+def print_last_digit(number);
+def add(a, b);
+def pow(a, b);
+def fizzbuzz();
+listint_t *insert_node(listint_t **head, int number);
+
+
+
+#endif /*LIST_H*/
